dc00f8015b921a264220e3af4b54889ce1a3ee03
Johan Ortega <hal215@outlook.com>
2021-06-21 22:19:18 -0500

    Modified:
    MangadexAPI.js
    - Fixed the issue with chapters being out of order... had to do with the call to the api

--- src/app/services/MangadexAPI.js
+++ src/app/services/MangadexAPI.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 // todo: move this to a config/appConfig.js
 const APP_CONFIG = {
-  apiUrl: "https://api.mangadex.org/"
+  apiUrl: "https://api.mangadex.org/",
 };
 
 class MangadexAPI {
   constructor() {
     this.api = axios.create({
-      baseURL: APP_CONFIG.apiUrl
+      baseURL: APP_CONFIG.apiUrl,
     });
   }
 
@@ -35,7 +35,7 @@ class MangadexAPI {
   }
   async fetchChapter(seriesId) {
     const response = await this.api.get(
-      `manga/${seriesId}/feed?translatedLanguage[]=en&order[volume]=desc`
+      `manga/${seriesId}/feed?translatedLanguage[]=en&order[chapter]=desc`
     );
 
     return response.data;
@@ -49,13 +49,13 @@ class MangadexAPI {
     return data;
   }
 
-   async fetchAuthor(authorID) {
+  async fetchAuthor(authorID) {
     const requestURL = `http://localhost:3001?query=author&ID=${authorID}`;
 
     console.log(authorID);
     console.log(requestURL);
 
-    return fetch(requestURL, { method: "GET" }).then(response =>
+    return fetch(requestURL, { method: "GET" }).then((response) =>
       response.json()
     );
     //console.log(response)
